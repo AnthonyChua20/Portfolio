0a0b5d89abcfad29957b44d3624d0f5eb1fedc9f
Anthony <Anthonychua2002@gmail.com>
2026-01-05 16:16:16 +0800

    Added the frontend for creating notes.

--- FrontEnd/src/pages/HomePage.jsx
+++ FrontEnd/src/pages/HomePage.jsx
@@ -2,9 +2,9 @@ import {useEffect, useState} from 'react'
 import Navbar from '../components/Navbar'
 import RateLimitedUi from '../components/RateLimitedUI'
 import { data } from 'react-router';
-import axios from "axios"
 import toast from "react-hot-toast"
 import NoteCard from '../components/NoteCard';
+import api from '../lib/axios';
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
@@ -14,7 +14,7 @@ const HomePage = () => {
   useEffect(()=> {
     const fetchNotes = async() =>{
       try {
-        const res = await axios.get("http://localhost:5000/api/notes")
+        const res = await api.get("/notes")
         console.log(res.data)
         SetNotes(res.data)
         setIsRateLimited(false)
