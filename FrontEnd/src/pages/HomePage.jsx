ecf12fa6a230488688738f9891add8d2fd888564
Anthony <Anthonychua2002@gmail.com>
2026-01-05 19:49:03 +0800

    Added some changes

--- FrontEnd/src/pages/HomePage.jsx
+++ FrontEnd/src/pages/HomePage.jsx
@@ -1,63 +1,59 @@
-import {useEffect, useState} from 'react'
-import Navbar from '../components/Navbar'
-import RateLimitedUi from '../components/RateLimitedUI'
-import { data } from 'react-router';
-import toast from "react-hot-toast"
-import NoteCard from '../components/NoteCard';
-import api from '../lib/axios';
-import NotesNotFound from '../components/NotesNotFound';
-
+import { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import RateLimitedUi from "../components/RateLimitedUI";
+import { data } from "react-router";
+import toast from "react-hot-toast";
+import NoteCard from "../components/NoteCard";
+import api from "../lib/axios";
+import NotesNotFound from "../components/NotesNotFound";
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
-  const [notes, SetNotes] = useState([])
+  const [notes, SetNotes] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  useEffect(()=> {
-    const fetchNotes = async() =>{
+  useEffect(() => {
+    const fetchNotes = async () => {
       try {
-        const res = await api.get("/notes")
-        console.log(res.data)
-        SetNotes(res.data)
-        setIsRateLimited(false)
-
-
+        const res = await api.get("/notes");
+        console.log(res.data);
+        SetNotes(res.data);
+        setIsRateLimited(false);
       } catch (error) {
-        console.log("Error fetching notes")
-        if(error.response.status == 429){
-          setIsRateLimited(true)
-          
-        } else{
-          toast.error("Failed to load notes")
+        console.log("Error fetching notes");
+        if (error.response.status == 429) {
+          setIsRateLimited(true);
+        } else {
+          toast.error("Failed to load notes");
         }
-      } finally{
+      } finally {
         setLoading(false);
       }
     };
 
     fetchNotes();
-  },[])
+  }, []);
 
   return (
-    <div className='min-h-screen'>
+    <div className="min-h-screen">
       <Navbar />
       {isRateLimited && <RateLimitedUi />}
-      <div className='max-w-7xl mx-auto p-4 mt-6'>
-        {loading && <div className='text-center text-primary py-10'>Loading Notes...</div>}
-        {notes.length === 0 && !isRateLimited  && <NotesNotFound />}
+      <div className="max-w-7xl mx-auto p-4 mt-6">
+        {loading && (
+          <div className="text-center text-primary py-10">Loading Notes...</div>
+        )}
+        {notes.length === 0 && !isRateLimited && <NotesNotFound />}
 
-
-
-        {notes.length>0 && !isRateLimited && (
-          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
-            {notes.map(note =>(
-              <NoteCard key ={note._id} note={note} SetNotes={SetNotes}/>
+        {notes.length > 0 && !isRateLimited && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {notes.map((note) => (
+              <NoteCard key={note._id} note={note} SetNotes={SetNotes} />
             ))}
           </div>
         )}
       </div>
-      </div>
-  )
-}
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
